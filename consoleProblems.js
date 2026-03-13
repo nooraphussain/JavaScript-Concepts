59951b922f38ffe8499bf9aabd9e7a8ee60f2989
nooraphussain <noorahussainp2002@gmail.com>
2026-03-13 11:54:15 +0530

    Object comparisons

--- consoleProblems.js
+++ consoleProblems.js
@@ -1,1 +1,11 @@
 console.log({} === {});
+console.log({} == {});
+
+//Objects are compared by reference, not by value.
+
+const user1 = { name: "Noorr" };
+const user2 = { name: "Noorr" };
+
+console.log(user1.name === user2.name);
+console.log(JSON.stringify(user1) === JSON.stringify(user2));
+//Two objects are equal only if they reference the SAME object in memory.
