3540838c6c6be333c762e32957e13569a41d3432
nooraphussain <noorahussainp2002@gmail.com>
2026-03-13 12:03:58 +0530

    Detailed concept

---
+++ primitiveNonPrimitive.js
@@ -0,0 +1,43 @@
+// Primitive Data Types
+// Definition
+
+// A primitive data type is:
+// A simple data value that stores the actual value directly in memory.
+
+// Number	10, 3.14
+// String	"hello"
+// Boolean	true, false
+// Undefined	undefined
+// Null	      null
+// Symbol	Symbol("id")
+// BigInt 123456789n
+
+//Because primitives are copied by value.
+
+// Primitive Characteristics
+
+// ✔ Stored directly in memory
+// ✔ Compared by value
+// ✔ Immutable
+// ✔ Fast and lightweight
+
+
+
+//Non-Primitive (Reference) Data Types
+// A non-primitive data type is:
+// A complex data structure that stores a reference (memory address) instead of the actual value.
+
+
+// Object	{name: "Noorr"}
+// Array	[1,2,3]
+// Function	function(){}
+// Date	new Date()
+// Map	new Map()
+// Set	new Set()
+
+
+// Non Primitive Characteristics
+
+// ✔ Stored reference in memory
+// ✔ Compared by reference
+// ✔ Mutable
